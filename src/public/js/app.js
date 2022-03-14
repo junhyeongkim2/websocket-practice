920bfe247af98ddc423a30a4023e7d69afbd879f
junhyeongkim2 <ggprgrkjh@naver.com>
2022-03-15 02:41:17 +0900

    feat:disconecting and message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -16,10 +16,24 @@ function addMessage(message) {
 
 room.hidden = true;
 
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("input");
+
+  const intputvalue = input.value;
+
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${intputvalue}`);
+  });
+  input.value = "";
+}
+
 function showRoom(msg) {
   welcome.hidden = true;
   room.hidden = false;
   console.log("entered room");
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(event) {
@@ -36,3 +50,9 @@ form.addEventListener("submit", handleRoomSubmit);
 socket.on("welcome", () => {
   addMessage("someone joined!");
 });
+
+socket.on("bye", () => {
+  addMessage("someone left!");
+});
+
+socket.on("new_message", addMessage);
